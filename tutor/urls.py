d9d2f64b6f9bde35317b57e96267fc2b56b27d1f
Filip Dimitrievski <f_dimitrievski@outlook.com>
2020-05-18 23:38:37 -0400

    Course home is ready. Detail course backend is working. TODO detail course frontend and QandA for course.

--- tutor/urls.py
+++ tutor/urls.py
@@ -5,4 +5,7 @@ from tutor import views
 
 urlpatterns = [
     path('', views.tutor_home, name='tutor_home'),
+    path('search', views.search_courses, name='search_courses'),
+    path('<int:pk>/detail', views.detail_course, name='detail_course'),
+    path('<int:pk>/<int:video_pk>/play_video', views.play_video, name='play_video'),
 ]
--- tutor/views.py
+++ tutor/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from tutor import models
+
 
 def tutor_home(request):
     """Show home page of Tutor"""
@@ -7,5 +9,69 @@ def tutor_home(request):
 
     passing_dict = {
         'courses': courses
+    }
+    return render(request, 'tutor/tutor_home.html', passing_dict)
+
+
+def detail_course(request, pk):
+    course = models.Course.objects.get(pk=pk)
+    videos = models.Video.objects.filter(course=course)
+
+    passing_dict = {
+        'course': course,
+        'videos': videos,
+        'show_video': videos[0],
+    }
+    return render(request, 'tutor/detail_course.html', passing_dict)
+
+
+def play_video(request, pk, video_pk):
+    course = models.Course.objects.get(pk=pk)
+    videos = models.Video.objects.filter(course=course)
+    show_video = models.Video.objects.get(pk=video_pk)
+
+    passing_dict = {
+        'course': course,
+        'videos': videos,
+        'show_video': show_video,
+    }
+    return render(request, 'tutor/detail_course.html', passing_dict)
+
+
+def search_courses(request):
+    """Find a course or video by user search value"""
+    courses = models.Course.objects.all()
+    search_result = []
+    num_of_results = 0
+
+    if not request.POST['search_value']:
+        return redirect('tutor_home')
+    for course_counter in courses:
+        temp_videos = models.Video.objects.filter(course=course_counter)
+        if str(course_counter).lower().find(request.POST['search_value'].lower()) != -1:
+            search_result.append(course_counter)
+            num_of_results += 1
+        else:
+            for video in temp_videos:
+                if str(video).lower().find(request.POST['search_value'].lower()) != -1:
+                    search_result.append(course_counter)
+                    num_of_results += 1
+
+    if search_result:
+        courses = search_result
+        message_type = 'success'
+        if num_of_results == 1:
+            message = ['{} results is found'.format(num_of_results)]
+        else:
+            message = ['{} results are found'.format(num_of_results)]
+    else:
+        message_type = 'error'
+        message = ["Unfortunately that content doesn't exists"]
+
+    passing_dict = {
+        'courses': courses,
+        'num_of_results': num_of_results,
+        'message_type': message_type,
+        'message': message
     }
     return render(request, 'tutor/tutor_home.html', passing_dict)
